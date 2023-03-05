ade322396dab3adf2ffe4e13f1e39945a77efaa7
FarkyBat <103525920+FarkyBat@users.noreply.github.com>
2023-03-05 09:30:02 +0100

    Add files via upload

---
+++ event.js
@@ -0,0 +1,66 @@
+function kesz(){
+    if (document.getElementById("t1").value=="alma") { //válasz 1
+        var x = document.getElementById("t1").value;
+        var image = document.createElement("IMG");
+        image.setAttribute("src", "firefox.png"); //kép.png vagy .egyéb
+        document.getElementById("td1").appendChild(image);
+        document.getElementById("t1").disabled = true;
+    }
+    if (document.getElementById("t2").value=="alma2") { //válasz 2
+        var x = document.getElementById("t2").value;
+        var image = document.createElement("IMG");
+        image.setAttribute("src", "file.png"); //kép.png vagy .egyéb
+        document.getElementById("td2").appendChild(image);
+        document.getElementById("t2").disabled = true;
+    }
+    if (document.getElementById("t3").value=="alma3") { //válasz 3
+        var x = document.getElementById("t3").value;
+        var image = document.createElement("IMG");
+        image.setAttribute("src", "code.png"); //kép.png vagy .egyéb
+        document.getElementById("td3").appendChild(image);
+        document.getElementById("t3").disabled = true;
+    }
+    if (document.getElementById("t4").value=="alma4") { //válasz 4
+        var x = document.getElementById("t4").value;
+        var image = document.createElement("IMG");
+        image.setAttribute("src", "firefox.png"); //kép.png vagy .egyéb
+        document.getElementById("td4").appendChild(image);
+        document.getElementById("t4").disabled = true;
+    }
+    if (document.getElementById("t5").value=="alma5") { //válasz 5
+        var x = document.getElementById("t5").value;
+        var image = document.createElement("IMG");
+        image.setAttribute("src", "firefox.png"); //kép.png vagy .egyéb
+        document.getElementById("td5").appendChild(image);
+        document.getElementById("t5").disabled = true;
+    }
+    if (document.getElementById("t6").value=="alma6") { //válasz 6
+        var x = document.getElementById("t6").value;
+        var image = document.createElement("IMG");
+        image.setAttribute("src", "firefox.png"); //kép.png vagy .egyéb
+        document.getElementById("td6").appendChild(image);
+        document.getElementById("t6").disabled = true;
+    }
+    if (document.getElementById("t7").value=="alma7") { //válasz 7
+        var x = document.getElementById("t7").value;
+        var image = document.createElement("IMG");
+        image.setAttribute("src", "firefox.png"); //kép.png vagy .egyéb
+        document.getElementById("td7").appendChild(image);
+        document.getElementById("t7").disabled = true;
+    }
+    if (document.getElementById("t8").value=="alma8") { //válasz 8
+        var x = document.getElementById("t8").value;
+        var image = document.createElement("IMG");
+        image.setAttribute("src", "firefox.png"); //kép.png vagy .egyéb
+        document.getElementById("td8").appendChild(image);
+        document.getElementById("t8").disabled = true;
+    }
+    if (document.getElementById("t9").value=="alma9") { //válasz 9
+        var x = document.getElementById("t9").value;
+        var image = document.createElement("IMG");
+        image.setAttribute("src", "firefox.png"); //kép.png vagy .egyéb
+        document.getElementById("td9").appendChild(image);
+        document.getElementById("t9").disabled = true;
+    }
+    //console.log(x);
+}
